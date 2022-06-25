439ac4ccf69b60e2a3f9360cd7a6a16f5e3e0787
glebite <glebite@gmail.com>
2022-06-25 15:13:47 -0400

    Updating - moving along tests are next

--- src/manager.py
+++ src/manager.py
@@ -8,14 +8,21 @@ class Manager:
     """
     """
     def __init__(self, config_file):
-        pass
+        self.config_file = config_file
+
+    def configure(self):
+        config = configparser.ConfigParser()
+        print(f'parsing {config.sections()=}')
 
 
-def main():
+def main(configuration_file):
     """
     """
     pass
 
 
 if __name__=="__main__":
-    main()
+    parser = argparse.ArgumentParser(description = 'House Manager Reporter System.')
+    parser.add_argument('config', help='path to the configuration file')
+    args = parser.parse_args()
+    main(args.config)
