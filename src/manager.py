cf32d2f6d728898c185043f23cba739dfe9421fb
glebite <glebite@gmail.com>
2022-06-25 14:53:09 -0400

    Adding onto things and writing tests.

---
+++ src/manager.py
@@ -0,0 +1,21 @@
+"""manager.py
+"""
+import configparser
+import argparse
+
+
+class Manager:
+    """
+    """
+    def __init__(self, config_file):
+        pass
+
+
+def main():
+    """
+    """
+    pass
+
+
+if __name__=="__main__":
+    main()
---
+++ src/tests/test_manager.py
@@ -0,0 +1,3 @@
+import pytest
+from manager import Manager
+
