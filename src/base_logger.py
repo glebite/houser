81c38cf0d3dc6b1318d63e2eea72003125432cb0
glebite <glebite@gmail.com>
2022-07-01 21:37:58 -0400

    Added logging in.

---
+++ src/base_logger.py
@@ -0,0 +1,5 @@
+import logging
+
+
+logger = logging
+logging.basicConfig(format='%(asctime)s - %(levelname)s %(filename)s:%(funcName)s:%(lineno)d - %(message)s', level=logging.INFO)
--- src/manager.py
+++ src/manager.py
@@ -4,12 +4,14 @@ import configparser
 import argparse
 import logging
 import sys
+from base_logger import logger
 
 
 class Manager:
     """
     """
     def __init__(self, config_file):
+        logger.info(f'Initializing... {config_file=}')
         self.config_file = config_file
         self.configuration = None
 
@@ -21,14 +23,16 @@ class Manager:
             print(err)
             sys.exit(1)
 
+
 def main(configuration_file):
     """
     """
-    pass
+    x = Manager(configuration_file)
 
 
 if __name__=="__main__":
+    logger.info("This is an info message")    
     parser = argparse.ArgumentParser(description = 'House Manager Reporter System.')
-    parser.add_argument('config', help='path to the configuration file')
+    parser.add_argument('--config', help='path to the configuration file')
     args = parser.parse_args()
     main(args.config)
